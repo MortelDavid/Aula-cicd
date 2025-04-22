f7a9455073ba06998b846a56fff06fb1aaf8e481
MortelDavid <morteldavi@gmail.com>
2025-04-22 20:08:06 -0300

    Update and rename GitHUB.C to teste.c

---
+++ teste.c
@@ -0,0 +1,11 @@
+#incluide <studio.h>
+#include<assert.h>
+void testar(){
+  //Função de teste simples
+  assert(1==1); // verifica se 1 é iguala 1
+  printf("Teste passou!\n");
+}
+int main(){
+  testar();
+  return o;
+}
